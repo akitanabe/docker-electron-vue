99104c14f3ce953a732a499750fea936b5f8d497
akitanabe <tanabe@determaind.biz>
2020-10-03 19:39:26 +0900

    test用のファイルを更新

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript',
+  testMatch: ['**/src/**/*.spec.ts'],
   transform: {
-    '^.+\\.vue$': 'vue-jest'
-  }
-}
+    '^.+\\.vue$': 'vue-jest',
+  },
+};
